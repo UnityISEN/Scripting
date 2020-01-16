ab0c99e0b2a2486aeb6664ec7c0b85704648a9b8
Lenophie <quasiphies@gmail.com>
2020-01-16 10:40:01 +0100

    Use Lerp in ScriptSphereController

--- Assets/Scripts/Controllers/ScriptSphereController.cs
+++ Assets/Scripts/Controllers/ScriptSphereController.cs
@@ -3,17 +3,27 @@
 namespace Controllers {
     public class ScriptSphereController : MonoBehaviour {
         private Transform sphereTransform;
-        private float sinParameter;
+        private float t;
+        private bool isAscending;
 
         private void Awake() {
             sphereTransform = transform;
-            sinParameter = 0f;
+            t = 0f;
+            isAscending = true;
         }
 
         private void Update() {
             Vector3 position = sphereTransform.position;
-            sinParameter += 0.01f;
-            sphereTransform.position = new Vector3(position.x, Mathf.Sin(sinParameter), position.z);
+
+            if (isAscending) {
+                t += 0.01f;
+                if (t >= 1f) isAscending = false;
+            } else {
+                t -= 0.01f;
+                if (t <= 0f) isAscending = true;
+            }
+            
+            sphereTransform.position = new Vector3(position.x, Mathf.Lerp(0f, 2f, t), position.z);
         }
     }
 }
