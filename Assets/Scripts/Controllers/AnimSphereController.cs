d722a1fe34c8de100931639d895b90f10cf2cca4
Lenophie <quasiphies@gmail.com>
2020-01-16 10:49:20 +0100

    Add AnimSphereController

---
+++ Assets/Scripts/Controllers/AnimSphereController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Controllers {
+    [RequireComponent(typeof(Animator))]
+    public class AnimSphereController : MonoBehaviour {
+        private Animator animator;
+
+        private void Awake() {
+            animator = GetComponent<Animator>();
+        }
+
+        private void Start() {
+            animator.SetBool("isWaiting", true);
+        }
+    }
+}
