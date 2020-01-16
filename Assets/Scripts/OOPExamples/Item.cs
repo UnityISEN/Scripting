d570099b8cb92fc66e08a0741351d78356d44bc7
Lenophie <quasiphies@gmail.com>
2020-01-16 11:47:11 +0100

    Add OOPExamples

---
+++ Assets/Scripts/OOPExamples/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOPExamples {
+    public class Item {
+        public readonly string Name;
+        private int quantity;
+
+        public Item(string name, int initialQuantity) {
+            if (initialQuantity < 0) throw new Exception("Initial quantity has to be positive!");
+
+            Name = name;
+            quantity = initialQuantity;
+        }
+
+        public void UseItem() {
+            if (quantity > 0) quantity--;
+            else throw new Exception("Can't use item");
+        }
+    }
+}
---
+++ Assets/Scripts/OOPExamples/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPExamples {
+    public class Player {
+        private float health; // From 0 to 100;
+        private readonly List<Item> items;
+
+        public Player() {
+            health = 100f;
+            items = new List<Item> {
+                new Item("Potion", 5),
+                new Item("Lapin", 1),
+                new Item("Épée", 1)
+            };
+        }
+        
+        public void DrinkPotion() {
+            foreach (Item item in items) {
+                if (item.Name == "Potion") {
+                    item.UseItem();
+                    health += 10f;
+                }
+            }
+        }
+
+        public void TakeDamage(float damage) {
+            if (damage <= 0f) throw new Exception("Damage can't be negative ! (well it is, but not THAT way)");
+            health -= damage;
+        }
+    }
+}
