a1789804f05e5cf7bc3cd0782c4542ca081a11c9
Lenophie <quasiphies@gmail.com>
2020-01-16 10:35:47 +0100

    Use Sin in ScriptSphereController

--- Assets/Scripts/Controllers/ScriptSphereController.cs
+++ Assets/Scripts/Controllers/ScriptSphereController.cs
@@ -3,14 +3,17 @@
 namespace Controllers {
     public class ScriptSphereController : MonoBehaviour {
         private Transform sphereTransform;
+        private float sinParameter;
 
         private void Awake() {
             sphereTransform = transform;
+            sinParameter = 0f;
         }
 
         private void Update() {
             Vector3 position = sphereTransform.position;
-            sphereTransform.position = new Vector3(position.x, position.y + 0.01f, position.z);
+            sinParameter += 0.01f;
+            sphereTransform.position = new Vector3(position.x, Mathf.Sin(sinParameter), position.z);
         }
     }
 }
