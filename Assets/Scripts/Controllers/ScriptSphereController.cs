94a2621921e331b873cb25ff4d6e9bad60598bb2
Lenophie <quasiphies@gmail.com>
2020-01-16 10:32:06 +0100

    Add ScriptSphereController

---
+++ Assets/Scripts/Controllers/ScriptSphereController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Controllers {
+    public class ScriptSphereController : MonoBehaviour {
+        private Transform sphereTransform;
+
+        private void Awake() {
+            sphereTransform = transform;
+        }
+
+        private void Update() {
+            Vector3 position = sphereTransform.position;
+            sphereTransform.position = new Vector3(position.x, position.y + 0.01f, position.z);
+        }
+    }
+}
